08c879a1a23704d07993b172cea9f374530d320f
Orhan Özbek <orhanoz91@gmail.com>
2016-09-08 18:16:25 +0300

    Update hata.ino

--- hata.ino
+++ hata.ino
@@ -1,18 +1,30 @@
-int led1=4;
-int led2=5;
-int basla=3;
-int dur=2;
+//tanımlamalar 
+int led1=4;//led
+int led2=5;//led
+int basla=3;  //??
+int dur=2; //interrupt pin
+
 void setup() {
-  // put your setup code here, to run once:
+  
 pinMode(led1,OUTPUT);
 pinMode(led2,OUTPUT);
-attachInterrupt(0,durdurma,CHANGE) ;
+//bir konuyu ilkkez araştırırken hep referans sayfalarında tanımlamaların nasıl yapıldıgına baklmalısın
+//örnegin https://www.arduino.cc/en/Reference/attachInterrupt
+//attachInterrupt(0,durdurma,CHANGE) ; bunu şu sekilde yazmanın tavsiye edildigini görebilirsin
+
+//attachInterrupt(digitalPinToInterrupt(pin), ISR, mode);	(recommended)
+//attachInterrupt(interrupt, ISR, mode);	(not recommended)
+//ama 2 türlüde calışır ayrıntılarını ve nedenlerini araştırabilirsin :D
+attachInterrupt(digitalPinToInterrupt(dur), durdurma, CHANGE);
+//bu nasıl calışacak ??
+//-->dur pini üzerinde herhangi bir change görülürse durdurma caliştirilacak
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //burada bi degişik birşey olmuş yapmak istedigin şumu
+  // eger başla pini high ise
 if(digitalRead(basla)==HIGH){
-  while(digitalRead(basla)==HIGH){}
+  //while(digitalRead(basla)==HIGH){} burada parantezler hatalı sonsuz loop ...
 digitalWrite(led1,HIGH);
 delay(1120);
 digitalWrite(led1,LOW);
@@ -21,7 +33,8 @@ digitalWrite(led2,HIGH);
 delay(1120);
 digitalWrite(led2,LOW);
 delay(1120);
-}}
+  }
+}
 
 void durdurma(){
   if(digitalRead(dur)==HIGH){
