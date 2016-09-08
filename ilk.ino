6dad59b57c30cfbb85ead8e27c9407e9305cad2d
Orhan Özbek <orhanoz91@gmail.com>
2016-09-08 17:30:28 +0300

    Create ilk.ino

---
+++ ilk.ino
@@ -0,0 +1,22 @@
+void setup() {
+// KARAŞİMŞEK
+pinMode(0,OUTPUT);
+pinMode(1,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+
+}
+
+void loop() {
+// put your main code here, to run repeatedly://
+digitalWrite(0,HIGH);
+digitalWrite(1,HIGH);
+delay(200);
+
+digitalWrite(2,HIGH);
+delay(160);
+
+digitalWrite(3,HIGH);
+delay(120);
+
+}
